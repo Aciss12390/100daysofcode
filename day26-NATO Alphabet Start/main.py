61a62b75de156089b4763e9711d980bb2bc39c03
Aciss12390 <jessicaczechumska@gmail.com>
2025-11-09 12:27:50 +0100

    Clean up main.py by removing unused code
    Removed commented-out code and TODOs related to looping through dictionaries and data frames.

--- day26-NATO Alphabet Start/main.py
+++ day26-NATO Alphabet Start/main.py
@@ -3,25 +3,8 @@ student_dict = {
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas as pd
 student_data_frame = pd.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
 
 alphabet = pd.read_csv("nato_phonetic_alphabet.csv",header=None, names=['letter', 'code'])
 
@@ -35,5 +18,3 @@ print(word_list)
 
 word_dict = [alphabet_dictionary[letter] for letter in word_list if letter in alphabet_dictionary.keys()]
 print(word_dict)
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
